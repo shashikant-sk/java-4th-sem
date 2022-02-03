a2fd41c75977b98aaec88412aaccfdbeaf1c9bd3
shashikant <shashikantshah540@gmail.com>
2022-02-03 16:37:53 +0530

    push

---
+++ 1st java lab/A.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class A {
+  public static void main(String[] args) 
+  {
+    Scanner input = new Scanner (System.in);
+    System.out.print("YOUR NAME: ");
+    String fname = input.next();
+    System.out.print("");
+
+    System.out.print("ROLL NO: ");
+    String Roll = input.next();
+    System.out.print("");
+
+    System.out.print("SECTION: ");
+    String sec = input.next();
+    System.out.print("");
+
+    System.out.print("Branch: ");
+    String bra = input.next();
+    System.out.print("");
+
+    
+  }
+}
