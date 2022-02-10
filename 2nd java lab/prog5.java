3645a227dd4ca3628964879d600467a197a1695a
shashikant <shashikantshah540@gmail.com>
2022-02-11 01:36:43 +0530

    push

--- 2nd java lab/prog5.java
+++ 2nd java lab/prog5.java
@@ -1,3 +1,7 @@
+// Write a program which will overload the area () method and display the area of a circle,
+// triangle and square as per user choice and user entered dimensions.
+
+
 public class prog5 {
     public static void main(String args[]) {
         OverloadDemo ob = new OverloadDemo();
