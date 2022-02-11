b4b9bac74934ecf71813243aa812edfebcbe1614
shashikant <shashikantshah540@gmail.com>
2022-02-11 22:51:04 +0530

    push

--- 2nd java lab/aa.java
+++ 2nd java lab/aa.java
@@ -1,33 +1,33 @@
 // 1. Find the largest among 3 user entered nos. at the command prompt using Java.
 
-import java.util.Scanner;
-public class aa
-{
-    public static void main(String[] args) 
-    {
-        int x, y, z;
-        Scanner s = new Scanner(System.in);
-        System.out.print("Enter the first number:");
-        x = s.nextInt();
-        System.out.print("Enter the second number:");
-        y = s.nextInt();
-        System.out.print("Enter the third number:");
-        z = s.nextInt();
-        if(x > y && x > z)
-        {
-            System.out.println("Largest number is:"+x);
-        }
-        else if(y > z)
-        {
-            System.out.println("Largest number is:"+y);
-        }
-        else
-        {
-            System.out.println("Largest number is:"+z);
-        }
+// import java.util.Scanner;
+// public class aa
+// {
+//     public static void main(String[] args) 
+//     {
+//         int x, y, z;
+//         Scanner s = new Scanner(System.in);
+//         System.out.print("Enter the first number:");
+//         x = s.nextInt();
+//         System.out.print("Enter the second number:");
+//         y = s.nextInt();
+//         System.out.print("Enter the third number:");
+//         z = s.nextInt();
+//         if(x > y && x > z)
+//         {
+//             System.out.println("Largest number is:"+x);
+//         }
+//         else if(y > z)
+//         {
+//             System.out.println("Largest number is:"+y);
+//         }
+//         else
+//         {
+//             System.out.println("Largest number is:"+z);
+//         }
  
-    }
-}
+//     }
+// }
 
 
 
@@ -69,27 +69,27 @@ public class aa
 
 
 
-// import java.util.Scanner;
+import java.util.Scanner;
 
-// public class aa {
-//     public static void main(String[] args){
-//         int x,y,z;
-//         Scanner sc =new Scanner(System.in);
-//         System.out.print("Enter the First number:= ");
-//         x=sc.nextInt();
-//         System.out.print("Enter the Second number:= ");
-//         y=sc.nextInt();
-//         System.out.print("Enter the Third number:= ");
-//         z=sc.nextInt();
-//         if (x>y && x>z){
-//             System.out.println("The Largest Number is : "+x);
-//         }
-//         else if(y > z)
-//         {
-//             System.out.println("The Largest Number is : "+y);
-//     }
-//     else{
-//         System.out.println("The Largest Number is : "+z);
-//         }
-//     }
-// }
+public class aa {
+    public static void main(String[] args){
+        int x,y,z;
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter the First number:= ");
+        x=sc.nextInt();
+        System.out.print("Enter the Second number:= ");
+        y=sc.nextInt();
+        System.out.print("Enter the Third number:= ");
+        z=sc.nextInt();
+        if (x>y && x>z){
+            System.out.println("The Largest Number is : "+x);
+        }
+        else if(y > z)
+        {
+            System.out.println("The Largest Number is : "+y);
+    }
+    else{
+        System.out.println("The Largest Number is : "+z);
+        }
+    }
+}
