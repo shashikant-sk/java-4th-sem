982468dac2a032f3b9879605278d5162ef2086f2
shashikant <shashikantshah540@gmail.com>
2022-02-24 17:56:13 +0530

    Update prog1.java

--- 4th java lab/prog1.java
+++ 4th java lab/prog1.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class prog1 {
 
-    public static void main(String args[]) {
+    public static void main(String args[])
+     {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter n: ");
         int n = sc.nextInt();
