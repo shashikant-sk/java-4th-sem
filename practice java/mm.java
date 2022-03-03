5ed1afcc2739524143d42959effb8cfbdd8ec877
shashikant <shashikantshah540@gmail.com>
2022-03-03 23:46:19 +0530

    push

--- practice java/mm.java
+++ practice java/mm.java
@@ -1,41 +1,11 @@
-class mm {
-
-    int i= 10;
-    
-    private int j = 20;
-    
-    class Inner{
-    
-    private int m= 100;
-    
-    }
-    
-    Inner obI = new Inner();
-    
-    public int getj() {return j;}
-    
-    void display () {
-    
-    System.out.println("Ml = "+ob1.m);}
-    
-    } 
-    class B extends mm (
-    
-    int total, 1-20;
-    
-    void sum() (
-    
-    total = super.i +getj();
-    
-    void display() (System.out.println("M2+ obI.m);}
-     float add (int a, float b) 
-     {return a+b;}
-    
-    } class Access1 (
-    
-    public static void main(String args[]) {
-    
-    A obA= new A();
-    obA.display();
+import java.util.*;
+public class mm {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();     // next print only one word we put "" Line "" its print whole sentance
+       
+        System.out.println(name);
+        System.out.println("--------------------");
+      
     }
 }
---
+++ practice java/tempCodeRunnerFile.java
@@ -0,0 +1,6 @@
+ // int a = 10;
+        // int b =25;
+        // int sum  =  a + b;
+        // int diff =  a - b;
+        // //System.out.println(sum);
+        // System.out.println(diff);
