7c469b0b8fe3ec4d68593e54086b949a6eb4666b
shashikant <shashikantshah540@gmail.com>
2022-03-03 14:42:41 +0530

    PUSH

---
+++ practice java/mm.java
@@ -0,0 +1,9 @@
+class mm {
+    public static void main(String[] args)
+    {
+        int a=1,b=2,c;
+        c=a+b;
+        System.out.println("sum is :"+c);
+        System.out.println("Hello world");
+    }
+}
