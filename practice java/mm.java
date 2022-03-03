62f728dd6a7fd473fae3d743f34afcc4e721d5df
shashikant <shashikantshah540@gmail.com>
2022-03-03 18:50:47 +0530

    Update mm.java

--- practice java/mm.java
+++ practice java/mm.java
@@ -1,9 +1,41 @@
 class mm {
-    public static void main(String[] args)
-    {
-        int a=1,b=2,c;
-        c=a+b;
-        System.out.println("sum is :"+c);
-        System.out.println("Hello world");
+
+    int i= 10;
+    
+    private int j = 20;
+    
+    class Inner{
+    
+    private int m= 100;
+    
+    }
+    
+    Inner obI = new Inner();
+    
+    public int getj() {return j;}
+    
+    void display () {
+    
+    System.out.println("Ml = "+ob1.m);}
+    
+    } 
+    class B extends mm (
+    
+    int total, 1-20;
+    
+    void sum() (
+    
+    total = super.i +getj();
+    
+    void display() (System.out.println("M2+ obI.m);}
+     float add (int a, float b) 
+     {return a+b;}
+    
+    } class Access1 (
+    
+    public static void main(String args[]) {
+    
+    A obA= new A();
+    obA.display();
     }
 }
