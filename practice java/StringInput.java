d23f5c382634d257f1b035772c40ea959028c279
shashikant <shashikantshah540@gmail.com>
2022-03-05 00:16:42 +0530

    push

--- practice java/StringInput.java
+++ practice java/StringInput.java
@@ -1,20 +1,23 @@
-import java.util.*;
-
- public class StringInput {
-     public static void main(String[] args) {
-         Scanner sc = new Scanner(System.in);
-         System.out.println("please enter the number of string you want to enter :");
-         String [] string = new String[sc.nextInt()];
-         sc.nextLine();
-         for(int i=0; i<str.length;i++){
-             string[i] = sc.nextLine();
-         }
-         System.out.println("\n you have enter :");
-          for(String str: string){
-
-          }
-          System.out.println("str");
-        }
-        }
-
-                    
+import java.util.Scanner;  
+public class MultipleStringInputExample1  
+{  
+public static void main(String[] args)   
+{  
+Scanner sc = new Scanner(System.in);  
+System.out.print("Please enter the number of strings you want to enter: ");   
+//takes an integer input         
+String[] string = new String [sc.nextInt()];      
+//consuming the <enter> from input above  
+sc.nextLine();   
+for (int i = 0; i < string.length; i++)   
+{  
+string[i] = sc.nextLine();  
+}  
+System.out.println("\nYou have entered: ");  
+//for-each loop to print the string  
+for(String str: string)   
+{  
+System.out.println(str);  
+}  
+}  
+}  
