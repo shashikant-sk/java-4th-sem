24a9e8d1419d1a50a4010119e406b10d995c455d
shashikant <shashikantshah540@gmail.com>
2022-03-04 23:54:22 +0530

    push

--- practice java/add.java
+++ practice java/add.java
@@ -1,12 +1,28 @@
-import java.util.*;
+// import java.util.*;
 
+// public class add {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+//         int sum = a + b ;
+//         System.out.println(sum);
+
+//     }
+// }
+
+
+import java.util.*;
 public class add {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
+        System.out.println("Eneter tha 1st value :");
+        int a=sc.nextInt();
+        System.out.println("Enetr the 2nd value :");
         int b = sc.nextInt();
-        int sum = a + b ;
-        System.out.println(sum);
-
+        System.out.println("----------------");
+        int c = a + b ;
+        System.out.println("sum is : "+c);
+        System.out.println("----------------");
     }
 }
