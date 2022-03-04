bb2fa4c508aab7b852a7e7943f3c4c2556913aa8
shashikant <shashikantshah540@gmail.com>
2022-03-05 00:15:34 +0530

    push

---
+++ practice java/StringInput.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+ public class StringInput {
+     public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         System.out.println("please enter the number of string you want to enter :");
+         String [] string = new String[sc.nextInt()];
+         sc.nextLine();
+         for(int i=0; i<str.length;i++){
+             string[i] = sc.nextLine();
+         }
+         System.out.println("\n you have enter :");
+          for(String str: string){
+
+          }
+          System.out.println("str");
+        }
+        }
+
+                    
