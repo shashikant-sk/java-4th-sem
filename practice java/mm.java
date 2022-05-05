f139997b3d1632854628b203e1b6a31dbfd8d72e
shashikant <shashikantshah540@gmail.com>
2022-05-05 14:01:46 +0530

    PUSH

--- practice java/mm.java
+++ practice java/mm.java
@@ -3,23 +3,30 @@
 import java.io.*;
 import java.util.*;
 public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
     
+    Scanner sc = new Scanner(System.in);
     System.out.println("Enter the file name :");
+    
     String fileName = sc.nextLine();
     System.out.println("Enter the starting index :");
+    
     int i = sc.nextInt();
+    
     System.out.println("Enter the ending index :");
+    
     int j = sc.nextInt();
     try {
         FileReader fr = new FileReader(fileName);
         BufferedReader br = new BufferedReader(fr);
+        
         String s = br.readLine();
         System.out.println("The string is : "+s);
         String sub = s.substring(i,j);
         System.out.println("The substring is : "+sub);
+        
         String rev = new StringBuffer(sub).reverse().toString();
         System.out.println("The reverse string is : "+rev);
+        
         if(sub.equals(rev)) {
             System.out.println("The string is palindrome");
         }
