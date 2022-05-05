dcfbd157cd6593983a853fb27d69ed662c6c4308
shashikant <shashikantshah540@gmail.com>
2022-05-05 12:44:24 +0530

    push

--- practice java/add.java
+++ practice java/add.java
@@ -1,15 +1,15 @@
-import java.util.*;
+// import java.util.*;
 
-public class add {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int sum = a + b ;
-        System.out.println(sum);
+// public class add {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+//         int sum = a + b ;
+//         System.out.println(sum);
 
-    }
-}
+//     }
+// }
 
 
 // import java.util.*;
@@ -26,3 +26,4 @@ public class add {
 //         System.out.println("----------------");
 //     }
 // }
+
